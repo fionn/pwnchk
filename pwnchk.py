6bb2e59dc86184d27e87f3ee32d3db60968f9dfe
Fionn Fitzmaurice <iamfionn@github.com>
2018-02-22 22:29:13 +0800

    fix import order

--- pwnchk.py
+++ pwnchk.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 
-import requests
 import getpass
 from hashlib import sha1
+import requests
 
 def query_pw(prefix):
     assert len(prefix) == 5
     api_url = "https://api.pwnedpasswords.com/range/{}"
     return requests.get(api_url.format(prefix)).iter_lines()
 
-def number_of_hits(pw):
-    prefix = pw[:5]
+def number_of_hits(pw_hash):
+    prefix = pw_hash[:5]
     for line in query_pw(prefix):
-        if pw == prefix + line.decode().split(":")[0]:
+        if pw_hash == prefix + line.decode().split(":")[0]:
             return int(line.decode().split(":")[1])
     return 0
 
 if __name__ == "__main__":
-    pw = sha1(getpass.getpass().encode()).hexdigest().upper()
-    hits = number_of_hits(pw)
+    pw_hash = sha1(getpass.getpass().encode()).hexdigest().upper()
+    hits = number_of_hits(pw_hash)
     if hits:
-        print("{} appears {} times".format(pw, hits))
+        print("{} appears {} times".format(pw_hash, hits))
     else:
         print("No matches")
 
