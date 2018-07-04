09f71ef7f788d2e72d60c1a7dc1d7c63f151fb4d
Fionn Fitzmaurice <iamfionn@github.com>
2018-07-05 00:50:42 +0800

    static typing, update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Check a password against a database of breaches.
 
-This queries Troy Hunt's [pwned passwords](https://haveibeenpwned.com/Passwords) API v2. The password never leaves your machine, only a prefix of the SHA1, thereby guaranteeing _k_-anonymity.
+This queries the [pwned passwords](https://haveibeenpwned.com/Passwords) API v2. The password never leaves your machine, only a prefix of the SHA1, which guarantees _k_-anonymity.
 
--- pwnchk.py
+++ pwnchk.py
@@ -2,25 +2,29 @@
 
 import getpass
 from hashlib import sha1
+from typing import Generator
 import requests
 
-def query_pw(prefix):
+def query_pw(prefix: str) -> Generator[bytes, None, None]:
     assert len(prefix) == 5
     api_url = "https://api.pwnedpasswords.com/range/{}"
     return requests.get(api_url.format(prefix)).iter_lines()
 
-def number_of_hits(pw_hash):
+def number_of_hits(pw_hash: str) -> int:
     prefix = pw_hash[:5]
     for line in query_pw(prefix):
         if pw_hash == prefix + line.decode().split(":")[0]:
             return int(line.decode().split(":")[1])
     return 0
 
-if __name__ == "__main__":
+def main() -> None:
     pw_hash = sha1(getpass.getpass().encode()).hexdigest().upper()
     hits = number_of_hits(pw_hash)
     if hits:
         print("{} appears {} times".format(pw_hash, hits))
     else:
         print("No matches")
+
+if __name__ == "__main__":
+    main()
 
